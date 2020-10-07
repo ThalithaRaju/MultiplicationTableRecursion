30b3a7a56c387b5a7c7b8767082821debb0a1264
ThalithaRaju <thalitharaju22@gmail.com>
2020-10-07 13:32:43 +0530

    recursion

---
+++ MultiplicationTableRecursion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTableRecursion
+{
+    class Program
+    {
+        static void Multiplication(int a,int b)
+        {
+            if (b > 10)
+                return;
+            Console.WriteLine(a + "*" + b + "=" + a * b);
+            Multiplication(a, b + 1);
+        }
+        static void Main(string[] args)
+        {
+            int a = 2;
+            Multiplication(a, 1);
+            Console.ReadKey();
+        }
+    }
+}
