ceb5d7736ff6e51c4916db5ac5617d94260b507f
ThalithaRaju <thalitharaju22@gmail.com>
2020-10-08 22:32:27 +0530

    Second commit

--- MultiplicationTableRecursion/Program.cs
+++ MultiplicationTableRecursion/Program.cs
@@ -9,17 +9,40 @@ namespace MultiplicationTableRecursion
 {
     class Program
     {
-        static void Multiplication(int a,int b)
+        //static void Multiplication(int a, int b)
+        //{
+        //    if (b > 10)
+        //        return;
+        //    Console.WriteLine(a + "*" + b + "=" + a * b);
+        //    Multiplication(a, b + 1);
+        //}
+        static void Multiplication(int a, int b)
         {
-            if (b > 10)
-                return;
-            Console.WriteLine(a + "*" + b + "=" + a * b);
-            Multiplication(a, b + 1);
+            if (b > 0)
+            {
+                Multiplication(a, b - 1);
+                Console.WriteLine(a + "*" + b + "=" + a * b);
+            }
+            return;
         }
+        //static void print(int a)
+        //{
+        //    if (a > 0)
+        //    {
+        //    print(a - 1);
+        //    Console.WriteLine(a + "");
+        //    }
+
+        //    return;
+        //}
+
         static void Main(string[] args)
         {
+            //int a = 2;
+            //Multiplication(a, 1);
+            //print(10);
             int a = 2;
-            Multiplication(a, 1);
+            Multiplication(a, 10);
             Console.ReadKey();
         }
     }
